e6bb91a0cc654cf29f5fb8fb6014775945c47bf9
Lukas <lukasvalaitis1@outlook.com>
2023-10-04 22:30:38 +0100

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,59 @@
-import math
+while True:
 
-class byte_calculator:
-    def __init__(self, input_type = "Bytes"):
+    menu_unit_options = {
+    "1": "kilobytes",
+    "2": "megabytes",
+    "3": "gigabytes",
+    "4": "terabytes",
+    "5": "petabytes"
+}
+    print("""
+1.Go to Calculator
+2.View previous calculations 
+""")
+    Selection_main=input("Select an option:\n")
+    if Selection_main =="1":
+        Answer=input("Enter yes to continue\n")
+    elif Selection_main =="2":
+        pass
+    else:
+        print("error, invalid input")
+    
+    if Answer == "yes":
+        print(menu_unit_options)
+    else:
+        print("error,invalid input")
 
-        self.input_type = input_type
+    Selection_unit = input("Select an option:\n")
+    if Selection_unit == "1":
+        print("Enter yes to continue")
+    elif Selection_unit == "2":
+        print("Enter yes to continue")
+    elif Selection_unit == "3":
+        print("Enter yes to continue")
+    elif Selection_unit == "4":
+        print("Enter yes to continue")
+    elif Selection_unit == "5":
+        print("Enter yes to continue")
+    else:
+        print("Error, invalid input")
+
+    number = int(input("Enter the amount"))
+
+    if Selection_unit == "1":
+        print(number * 1024)
+    elif Selection_unit == "2":
+        print(number * (1024 ** 2) )
+    elif Selection_unit == "3":
+        print(number * (1024 ** 3) )
+    elif Selection_unit == "4":
+        print(number * (1024 ** 4))
+    elif Selection_unit == "5":
+        print(number * (1024 ** 5))
+    else:
+        print("Error, invalid input")
+    if not input("Do you want to continue? (y/n) ").lower().startswith("y"):
+        break
+    else:
+        continue
+
