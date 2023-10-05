bc66710ea61d4e07e5b395567f8599b6e1d1c99b
dominik <domgryko@gmail.com>
2023-10-05 20:09:23 +0100

    name changes

--- data_calculator.py
+++ data_calculator.py
@@ -9,18 +9,7 @@ class data_calculator:
         self.conversion_value = None #used to calculate the value that will be used to find the final value.
         self.byte_value = None
 
-        self.calculate_bytes()
-
-    
-    def find_conversion_type(self):
-        if self.conversion_type.lower() == "binary": self.conversion_value = 1024
-        elif self.conversion_type.lower() == "decimal": self.conversion_value = 1000
-        else: print("ERROR: Incorrect Conversion Type.")
-    
-    
-    def calculate_bytes(self):
         unit_value = 0 #this will be used to calculate the final value into bytes
-        temp_value = self.value
 
         if self.conversion_value == None:
             self.find_conversion_type()
@@ -35,29 +24,36 @@ class data_calculator:
             unit_value = self.conversion_value ** 4
         elif self.unit_type in ["petabytes", "pb"]: 
             unit_value = self.conversion_value ** 5
-
-        #temp_value = temp_value * (self.conversion_value ** )
-
-        #self.byte_value = temp_value    
+ 
         self.byte_value = self.value * unit_value
+        
+
+    
+    def find_conversion_type(self):
+        if self.conversion_type.lower() == "binary": self.conversion_value = 1024
+        elif self.conversion_type.lower() == "decimal": self.conversion_value = 1000
+        else: print("ERROR: Incorrect Conversion Type.")
+    
+    
+    def convert_to_bytes(self):
         return self.byte_value
     
-    def calculate_kilobytes(self):
+    def convert_to_kilobytes(self):
         if self.byte_value == None: self.calculate_bytes()
         return self.byte_value / self.conversion_value
 
-    def calculate_megabytes(self):
+    def convert_to_megabytes(self):
         if self.byte_value == None: self.calculate_bytes()
         return self.byte_value / (self.conversion_value ** 2)
     
-    def calculate_gigabytes(self):
+    def convert_to_gigabytes(self):
         if self.byte_value == None: self.calculate_bytes()
         return self.byte_value / (self.conversion_value ** 3)
 
-    def calculate_terrabytes(self):
+    def convert_to_terrabytes(self):
         if self.byte_value == None: self.calculate_bytes() 
         return self.byte_value / (self.conversion_value ** 4)
 
-    def calculate_petabytes(self):
+    def convert_to_petabytes(self):
         if self.byte_value == None: self.calculate_bytes() 
         return self.byte_value / (self.conversion_value ** 5)
